a03d11bb6c316dc08dacde3f352f2e8ee9bf5a57
VerdoodtDries <dries@nordend.eu>
2024-05-03 17:50:37 +0200

    Further development

--- geom_corrections/generate_jwt.py
+++ geom_corrections/generate_jwt.py
@@ -1,33 +1,31 @@
-import json
-from jwt.jwt import JWT
-from jwt.jwk import jwk_from_dict
+from jwt import JWT, jwk_from_dict
 import time
 import uuid
-
+import json
 
 def generate_jwt(client_id: str, jwk_private_key_file: str):
     print(f"gebruiken van JWK private key in bestand {jwk_private_key_file}")
     with open(jwk_private_key_file) as key_file:
         json_data = json.load(key_file)
         private_key = jwk_from_dict(json_data)
-        print("private key is aangemaakt")
-        print("Aanmaken van een jwt token")
-        current_time_in_seconds = round(time.time())
-        expiry_time_in_seconds = current_time_in_seconds + 600
-        claims = {
-            "exp": expiry_time_in_seconds,
-            "iat": current_time_in_seconds,
-            # dit is een random token om replay attacks te vermijden
-            "jti": str(uuid.uuid4()),
-            "iss": client_id,
-            "sub": client_id,
-            "aud": "https://authenticatie.vlaanderen.be/op"
-        }
-        print(f"de claims zijn {claims}")
-        header = {
-            "alg": "RS256",
-            "typ": "JWT"
-        }
-        signed_jwt = JWT().encode(claims, private_key, alg="RS256", optional_headers=header)
-        print(f"signed JWT token : {signed_jwt}")
+        print(f"private key is aangemaakt: {private_key}")
+    print("Aanmaken van een jwt token")
+    current_time_in_seconds = round(time.time())
+    expiry_time_in_seconds = current_time_in_seconds + 600
+    claims = {
+        "exp": expiry_time_in_seconds,
+        "iat": current_time_in_seconds,
+        # dit is een random token om replay attacks te vermijden
+        "jti": str(uuid.uuid4()),
+        "iss": client_id,
+        "sub": client_id,
+        "aud": "https://authenticatie.vlaanderen.be/op"
+    }
+    print(f"de claims zijn {claims}")
+    header = {
+        "alg": "RS256",
+        "typ": "JWT"
+    }
+    signed_jwt = JWT().encode(claims, private_key, alg="RS256", optional_headers=header)
+    print(f"signed JWT token : {signed_jwt}")
     return signed_jwt
--- geom_corrections/get_token.py
+++ geom_corrections/get_token.py
@@ -1,34 +1,58 @@
 import json
 import os
 import requests
-from generate_jwt import generate_jwt  # Roep deze functie aan!
+#from generate_jwt import generate_jwt  # Roep deze functie aan!
+from jwt import JWT, jwk_from_dict
+import time
+import uuid
+from get_iat import get_iat
 
-
-# Define functions
-
-# Roep een functie aan om de Token op te halen
-# input: client_id, environment,
-# output: token
 environment_name = 't&i'
-
-settings_file = os.path.join(r'C:\Users\DriesVerdoodtNordend\OneDrive - Nordend\projects\AWV\python_repositories\OTL_Corrector\settings.json')
+settings_file = os.path.join('../settings.json')
 
 # Read settings file and obtain the client_id
 with open(settings_file) as f:
     json_data = json.load(f)
-#print('Settings file content: {0}'.format(json_data))
 
-# Retrieve the client_id from the specified environment
+# Given a specific environment, retrieve the client_id, jwk_private_key_file, environment_url
 client_id = next((auth_option['client_id'] for auth_option in json_data['auth_options'] if
                   auth_option['environment'] == environment_name), None)
 jwk_private_key_file = next((auth_option['key_path'] for auth_option in json_data['auth_options'] if
                  auth_option['environment'] == environment_name), None)
-environment_url = next((auth_option['environment_url'] for auth_option in json_data['auth_options'] if
-                        auth_option['environment'] == environment_name), None)
+#environment_url = next((auth_option['environment_url'] for auth_option in json_data['auth_options'] if auth_option['environment'] == environment_name), None)
 
+##
+##client_assertion = generate_jwt(client_id, jwk_private_key_file)
+##print(f"The signed JSON Web Token (JWT), also named client_assertion is: {client_assertion}")
 
-client_assertion = generate_jwt(client_id, jwk_private_key_file)
-print(f"The signed JSON Web Token (JWT), also named client_assertion is: {client_assertion}")
+sub = client_id
+iat = get_iat()
+exp = iat + 599
+jti = str(uuid.uuid4())
+# environment_url = feature.getAttribute('environment_url')
+instance = JWT()
+aud = "https://authenticatie.vlaanderen.be/op"
+
+with open(jwk_private_key_file) as pk:
+    json_data = json.load(pk)
+
+private_key = jwk_from_dict(json_data)
+
+payload = {
+    "exp": 1714748919,
+    "iat": 1714748321,
+    "jti": jti,
+    "iss": sub,
+    "sub": sub,
+    "aud": aud,
+}
+
+header = {
+    "alg": "RS256",
+    "typ": "JWT"
+}
+
+client_assertion = instance.encode(payload, private_key, alg='RS256', optional_headers=header)
 
 # Launch API Call POST
 response = requests.post(
@@ -44,20 +68,8 @@ response = requests.post(
     }
 )
 
+# Compare FME and Python Post requests with a tool like Fiddler
+
 print(f"resultaat van het opvragen van assets : {response.status_code}, {response.text}")
 print("Ophalen token ")
 print(response)
-
-
-
-def getToken(settings_file, environment, private_key):
-    """
-
-    :return: token
-    """
-    # Read settings file
-
-    # Get the client_id based on the environment
-
-    # Get the private keys
-    return token
