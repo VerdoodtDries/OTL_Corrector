9c7b11de69e3b8c1be9f350933e4925a42e0625a
VerdoodtDries <dries@nordend.eu>
2024-07-17 09:29:27 +0200

    Update rapport0133: dubbele assets

--- Report0133/Report0133.py
+++ Report0133/Report0133.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from otlmow_converter.OtlmowConverter import OtlmowConverter
 from otlmow_model.OtlmowModel.Helpers.OTLObjectHelper import print_overview_assets
 from otlmow_converter.FileFormats.PandasConverter import PandasConverter
+from fuzzywuzzy import fuzz
+from HelperFunctions.utils import split_list
 import pandas as pd
 import geopandas as gpd
 from shapely import wkt
@@ -20,6 +22,12 @@ import matplotlib.pyplot as plt
 matplotlib.use('TkAgg')
 import contextily as ctx
 from matplotlib.ticker import ScalarFormatter
+import os
+
+## Variables
+## filename = r"C:\Users\DriesVerdoodtNordend\OneDrive - Nordend\projects\AWV\python_repositories\OTL_Corrector\Report0133\Report0133_WestVL_18_44.csv"
+filename = r"C:\Users\DriesVerdoodtNordend\OneDrive - Nordend\projects\AWV\python_repositories\OTL_Corrector\Report0133\[RSA] Dubbele bomen_20240716.xlsx"
+feature_type = 'Resultaat' # This is the layer name or the Excel sheet name
 
 
 if __name__ == '__main__':
@@ -39,36 +47,39 @@ if __name__ == '__main__':
 
 ## Functions
 
+# Read the Sqlite file in a pandas dataframe
+script_dir = os.path.dirname(os.path.abspath(__file__))
+filepath = os.path.join(script_dir, filename)
 
-# Read the Excel file in a dataframe
-excel_path = r"C:\Users\DriesVerdoodtNordend\OneDrive - Nordend\projects\AWV\python_repositories\OTL_Corrector\Report0133\RSA_Report0133_afstand1meter_Vlaanderen.xlsx"
-df_report0133 = pd.read_excel(excel_path, sheet_name='RSA_Report0133_afstand1meter_Vl', header=0)
-print(df_report0133)
+try:
+    # Split the extension
+    filename_abs, extension = os.path.splitext(filename)
+    # Read the data from the SQLite database into a geopandas geodataframe
+    if extension == '.sqlite':
+        gdf_input = gpd.read_file(filepath, layer=feature_type)
+    # Read the data from an Excel file into a pandas dataframe
+    elif extension == '.xlsx':
+        df_input = pd.read_excel(filepath, sheet_name=feature_type, skiprows=2)
+    elif extension == '.csv':
+        df_input = pd.read_csv(filepath_or_buffer=filepath, delimiter=';')
+except FileNotFoundError as e:
+    raise FileNotFoundError(f"The file {filepath} does not exist.") from e
 
 # Launch the API request to obtain data
-typeURI = 'https://wegenenverkeer.data.vlaanderen.be/ns/onderdeel#Boom'
-boom1_uuid = df_report0133['boom1_uuid'].tolist()
-boom2_uuid = df_report0133['boom2_uuid'].tolist()
-print(f'typeURI: {typeURI}')
-print(f'boom1 uuid: {boom1_uuid}')
-print(f'boom2 uuid: {boom2_uuid}')
+asset_uuids = df_input['boom1_uuid'].tolist()
+asset_uuids = [i[:36] for i in asset_uuids] # Behoud enkel de eerste 36 karakters, hetgeen overeenkomt met de uuid, en niet de volledige AIM-ID
 
-# Development: Sample de eerste boom
-# Sample the first element of the list
-boom1_uuid_sample = [boom1_uuid[0]]
-boom2_uuid_sample = [boom2_uuid[0]]
-print(f'Sample boom1 uuid: {boom1_uuid_sample}')
-print(f'Sample boom2 uuid: {boom2_uuid_sample}')
+# Launch the API request to obtain data
+asset_uuids_bin100 = split_list(asset_uuids)
+asset_dicts_dict = {}  # Instantiate an empty dictionary to fill later on
+for bin_asset_uuids in asset_uuids_bin100:
+    # Bewaar de resultaten per match
+    object_generator = eminfra_importer.import_assets_from_webservice_by_uuids(asset_uuids=bin_asset_uuids)
+    print(f'Type: {type(object_generator)}\nResponse: {object_generator}')
+    asset_dicts_dict |= AssetUpdater.get_dict_from_object_generator(object_generator)  ## |= is the update operator for dictionaries.
 
-
-object_generator = eminfra_importer.import_assets_from_webservice_by_uuids(asset_uuids=boom1_uuid_sample)
-print(f'Type: {type(object_generator)}\nResponse: {object_generator}')
-
-#asset_dicts_dict = AssetUpdater.get_dict_from_object_generator(object_generator)
-asset_dicts_dict = AssetUpdater.get_dict_from_object_generator(object_generator)
-
-
-# Convert the data to a dataframe
+# Convert the data to a dataframe. Add the column "sorted_uuids".
+# This is the column to detect the duplicate assets (identical trees).
 ##df = pd.DataFrame.from_dict(data=asset_dicts_dict)
 df = pd.DataFrame(data=asset_dicts_dict)
 # Transpose rows and column
@@ -84,39 +95,78 @@ print(f'Geoseries: {gs_geometry}')
 # Convert the DataFrame to a GeoDataFrame
 gdf = gpd.GeoDataFrame(df, geometry=gs_geometry, crs="EPSG:31370")
 
+# Step 1: Merge the input dataframe (df_input) and add the column "boom2_uuid".
+# preprocess df_input: set index, keep only boom1_uuid and boom2_uuid
+# Keep a minimalistic version of df_input
+df_input_minimal = df_input[["boom1_uuid", "boom2_uuid"]].copy()
+df_input_minimal.set_index("boom1_uuid", drop=False, inplace=True)
+gdf = pd.merge(gdf, df_input_minimal, left_index=True, right_index=True, how='left')
+# Step 2: Create a sorted tuple for each combination of 'uuid1' and 'uuid2'
+#         Add a column that indicates the match between two assets: id_duplicate_asset
+gdf['id_duplicate_asset'] = gdf.apply(lambda row: tuple(sorted([row['boom1_uuid'], row['boom2_uuid']])), axis=1)
 
-# Visualise the gdf in a small map
-## to do
-# Convert to Web Mercator (EPSG:3857) for compatibility with contextily
-gdf_wm = gdf.to_crs(epsg=3857) # Geodataframe Webmercator
+# Sort the dataframe for visual inspection
+gdf.sort_values(by="id_duplicate_asset", inplace=True)
 
-# Plot the GeoDataFrame
-fig, ax = plt.subplots(figsize=(10, 10))
 
-gdf_wm.plot(ax=ax, color='green', legend=True)
+# Oplijsten van de attributen waarop een score wordt berekend. Verhoog met 1 als er een waarde beschikbaar is.
+lst_attributen_score = ["AIMNaamObject.naam", "VegetatieElement.hoogte", "AIMObject.datumOprichtingObject", "Boom.heeftLuchtleiding", "VegetatieElement.soortnaam", "Boom.boomspiegel", "Boom.eindbeeld"]  # "AIMObject.notitie"
+# Bereken de fuzzystring matching score. Dit percentage duidt de gelijkenis tussen twee attributen aan. Controleer of bepaalde attributen ongeveer gelijkaardig zijn.
+lst_attributen_fuzzystring =["VegetatieElement.soortnaam"]
+# Bereken of bepaalde attributen tegenstrijdige informatie bevatten. Dit zijn kritieke attributen die identieke moeten zijn.
+lst_attributen_tegenstrijdig = ["AIMNaamObject.naam"]  # Kritieke attributen waarvoor we detecteren of ze tegenstrijdig zijn.
 
-# Disable scientific notation and use plain style
-formatter = ScalarFormatter(useOffset=False, useMathText=True)
-formatter.set_scientific(False)
+# Instantiate attributes
+gdf['score_nbr_attributes'] = 0  ## Integer
+gdf['score_fuzzystringmatch'] = 0.0  ## Float
+gdf['score_tegenstrijdige_attr'] = False  ## Boolean
 
-# Optionally, set the number of ticks to show a better representation of full numbers
-ax.xaxis.set_major_formatter(formatter)
-ax.yaxis.set_major_formatter(formatter)
+# Outer loop in the dataframe
+for i in range(0, len(gdf), 2):
+    row1 = gdf.iloc[i]
+    row2 = gdf.iloc[i+1]
+    row1 = row1.fillna(value='')
+    row2 = row2.fillna(value='')
 
-# Crop the figure
-minx, miny, maxx, maxy = gdf_wm.buffer(distance=100).total_bounds # 100 meter buffer rondom het gdf om de bounding extent te bepalen
-ax.set_xlim(minx, maxx)
-ax.set_ylim(miny, maxy)
+    # Inner loop to calculate scores
+    # Loop to calculate the score. Score indicates the number of attributes that have a value
+    score1 = 0
+    score2 = 0
+    for attr_name in lst_attributen_score:
+        if row1[attr_name] and row1[attr_name] != '':  # Has a value
+            score1 += 1
+        if row2[attr_name] and row2[attr_name] != '':  # Has a value
+            score2 += 1
+    column_index = gdf.columns.get_loc('score_nbr_attributes')
+    gdf.iloc[i, column_index] = score1
+    gdf.iloc[i+1, column_index] = score2
 
-# Add the OpenStreetMap basemap
-ctx.add_basemap(ax=ax, source=ctx.providers.OpenStreetMap.Mapnik)
+    # Loop to calculate the fuzzymatch score. Percentage indicates similarity between values
+    for attr_name in lst_attributen_fuzzystring:
+        value1 = str(row1[attr_name])
+        value2 = str(row2[attr_name])
+        partial_ratio = fuzz.partial_ratio(value1, value2)
 
-# Save the plot as a PNG image
-plt.title('Dubbele bomen')
-plt.savefig("Report0133.png", dpi=300)
+    column_index = gdf.columns.get_loc('score_fuzzystringmatch')
+    gdf.iloc[i, column_index] = partial_ratio
+    gdf.iloc[i+1, column_index] = partial_ratio
 
-# tot hier
-# Add the second tree
-# Add an appropriate title (both uuids) and a legend.
+    # Loop to calculate tegenstrijdige attributen
+    for attr_name in lst_attributen_tegenstrijdig:
+        value1 = row1[attr_name]
+        value2 = row2[attr_name]
+        # Controle: de waarden zijn tegenstrijdig
+        column_index = gdf.columns.get_loc('score_tegenstrijdige_attr')
+        if value1 != value2:
+            gdf.iloc[i, column_index] = True
+            gdf.iloc[i+1, column_index] = True
 
-# Convert to JSON
+
+print('tot hier')
+
+# Kolom toevoegen: record behouden. Voor iedere combinatie (id_duplicate_asset) eenmaal True en eenmaal False toevoegen.
+
+# Wegschrijven naar een JSON dataframe om objecten te deactiveren.
+
+
+
